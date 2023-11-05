f9164d2e282cfff3307e8cc698678630a5a98b5f
Zenon Seth <Zenon.Seth@gmail.com>
2023-11-05 07:01:30 +0000

    Cleanup network logic file a bit, improve efficiency slightly

--- logic/network_logic.lua
+++ logic/network_logic.lua
@@ -3,7 +3,6 @@ local HARD_NETWORK_NODE_LIMIT = 1000 -- A network cannot consist of more than th
 local STATUS_OK = 0
 local CREATE_NETWORK_STATUS_FAIL_OTHER_NETWORK = -1
 local CREATE_NETWORK_STATUS_TOO_MANY_NODES = -2
-local ADD_STORAGE_MULTIPLE_NETWORKS = -1
 
 logistica.networks = networks
 
@@ -21,26 +20,14 @@ local adjecent = {
 
 local function has_machine(network, id)
   if not network then return false end
-  if network.demanders and network.demanders[id] then
-    return true
-  elseif network.suppliers and network.suppliers[id] then
-    return true
-  elseif network.mass_storage and network.mass_storage[id] then
+  if network.demanders[id] or network.suppliers[id] or network.mass_storage[id] or network.item_storage[id]
+  then
     return true
   else
     return false
   end
 end
 
-function logistica.get_network_name_or_nil(pos)
-  local hash = p2h(pos)
-  for nHash, network in pairs(networks) do
-    if hash == nHash then return network.name end
-    if network.cables[hash] then return network.name end
-    if has_machine(network, hash) then return network.name end
-  end
-  return nil
-end
 
 function logistica.get_network_or_nil(pos)
   local hash = p2h(pos)
@@ -52,6 +39,12 @@ function logistica.get_network_or_nil(pos)
   return nil
 end
 
+function logistica.get_network_name_or_nil(pos)
+  local network = logistica.get_network_or_nil(pos)
+  if not network then return nil else return network.name end
+end
+
+
 function logistica.get_network_id_or_nil(pos)
   local network = logistica.get_network_or_nil(pos)
   if not network then return nil else return network.controller end
@@ -61,33 +54,6 @@ end
 ----------------------------------------------------------------
 -- Network operation functions
 ----------------------------------------------------------------
-
-local function dumb_remove_from_network(networkName, pos)
-  local network = networks[networkName]
-  if not network then return false end
-  local hashedPos = p2h(pos)
-  if network.cables[hashedPos] then
-    network.cables[hashedPos] = nil
-    return true
-  end
-  if network.mass_storage[hashedPos] then
-    network.mass_storage[hashedPos] = nil
-    return true
-  end
-  if network.demanders[hashedPos] then
-    network.demanders[hashedPos] = nil
-    return true
-  end
-  if network.suppliers[hashedPos] then
-    network.suppliers[hashedPos] = nil
-    return true
-  end
-  if network.controller == hashedPos then
-    networks[networkName] = nil -- removing the controller removes the whole network
-    return true
-  end
-  return false
-end
 
 local function clear_network(networkName)
   local network = networks[networkName]
@@ -106,6 +72,23 @@ function logistica.updateStorageCacheFromPosition(pos)
   if network then
     logistica.updateStorageCache(network)
   end
+end
+
+-- returns a naturally numbered list of networks on adjecent nodes
+local function find_adjecent_networks(pos)
+  local connectedNetworks = {}
+  for _, adj in pairs(adjecent) do
+    local otherPos = vector.add(pos, adj)
+    local otherNetwork = logistica.get_network_id_or_nil(otherPos)
+    if otherNetwork then
+      connectedNetworks[otherNetwork] = true
+    end
+  end
+  local retNetworks = {}
+  for k,_ in pairs(connectedNetworks) do
+    table.insert(networks[k])
+  end
+  return retNetworks
 end
 
 --[[ updates the storage cach which holds where items may be found
@@ -133,83 +116,64 @@ function logistica.updateStorageCache(network)
   end
 end
 
-local function dumb_add_storage_to_network(networkId, pos)
-  local network = networks[networkId]
-  if not network then return false end
-  local node = minetest.get_node(pos)
-  local hashedPos = p2h(pos)
-  if logistica.is_mass_storage(node.name) then
-    network.mass_storage[hashedPos] = true
-    return true
-  elseif logistica.is_item_storage(node.name) then
-    network.item_storage[hashedPos] = true
-  else -- can't dumb-add other things
-    return false
-  end
-end
-
-local function dumb_add_cable_to_network(networkName, pos)
-  local network = networks[networkName]
-  if not network then return false end
-  local node = minetest.get_node(pos)
-  local hashedPos = p2h(pos)
-  if logistica.is_cable(node.name) then
-    network.cables[hashedPos] = true
-    return true
-  else -- can't dumb-add other things
-    return false
-  end
-end
-
-local function recursive_scan_for_nodes_for_controller(network, positions, numScanned)
-  if #positions <= 0 then return STATUS_OK end
-  if not numScanned then numScanned = #positions
-  else numScanned = numScanned + #positions end
+local function recursive_scan_for_nodes_for_controller(network, positionHashes, numScanned)
+  if not numScanned then numScanned = 0 end
 
   if numScanned > HARD_NETWORK_NODE_LIMIT then
     return CREATE_NETWORK_STATUS_TOO_MANY_NODES
   end
-
+  
   local connections = {}
-  for _, pos in pairs(positions) do
+  local newToScan = 0
+  for posHash, _ in pairs(positionHashes) do
+    local pos = h2p(posHash)
+    numScanned = numScanned + 1
     logistica.load_position(pos)
     local tiers = logistica.get_item_tiers(minetest.get_node(pos).name)
     local isAllTier = tiers[logistica.TIER_ALL] == true
     for _, offset in pairs(adjecent) do
       local otherPos = vector.add(pos, offset)
       logistica.load_position(otherPos)
+      local otherNode = minetest.get_node(otherPos)
       local otherHash = p2h(otherPos)
-      local tiersMatch = isAllTier
-      if tiersMatch ~= true then
-        local otherTiers = logistica.get_item_tiers(minetest.get_node(otherPos).name)
-        tiersMatch = logistica.do_tiers_match(tiers, otherTiers)
-      end
-      if tiersMatch
-        and network.controller ~= otherHash
-        and not has_machine(network, otherHash)
-        and network.cables[otherHash]  == nil then
-        local otherNode = minetest.get_node(otherPos)
-        if logistica.is_cable(otherNode.name) then
-          local existingNetwork = logistica.get_network_id_or_nil(otherPos)
-          if existingNetwork then
-            return CREATE_NETWORK_STATUS_FAIL_OTHER_NETWORK
-          else
-            network.cables[otherHash] = true
-            table.insert(connections, otherPos)
-          end
-        elseif logistica.is_demander(otherNode.name) then
-          network.demanders[otherHash] = true
-        elseif logistica.is_supplier(otherNode.name) then
-          network.suppliers[otherHash] = true
-        elseif logistica.is_mass_storage(otherNode.name) then
-          network.mass_storage[otherHash] = true
-          dumb_add_storage_to_network(network.controller, otherPos)
+      if network.controller ~= otherHash
+          and not has_machine(network, otherHash)
+          and network.cables[otherHash] == nil then
+        local tiersMatch = isAllTier
+        if tiersMatch ~= true then
+          local otherTiers = logistica.get_item_tiers(minetest.get_node(otherPos).name)
+          tiersMatch = logistica.do_tiers_match(tiers, otherTiers)
         end
-      end
+        if tiersMatch then
+          local existingNetwork = logistica.get_network_id_or_nil(otherPos)
+          if existingNetwork ~= nil and existingNetwork ~= network then
+            return CREATE_NETWORK_STATUS_FAIL_OTHER_NETWORK
+          end
+          if logistica.is_cable(otherNode.name) then
+            network.cables[otherHash] = true
+            connections[otherHash] = true
+            newToScan = newToScan + 1
+          elseif logistica.is_demander(otherNode.name) then
+            network.demanders[otherHash] = true
+            newToScan = newToScan + 1
+          elseif logistica.is_supplier(otherNode.name) then
+            network.suppliers[otherHash] = true
+            newToScan = newToScan + 1
+          elseif logistica.is_mass_storage(otherNode.name) then
+            network.mass_storage[otherHash] = true
+            newToScan = newToScan + 1
+          elseif logistica.is_item_storage(otherNode.name) then
+            network.item_storage[otherHash] = true
+            newToScan = newToScan + 1
+          end
+        end -- end if tiersMatch
+      end -- end of general checks
     end -- end inner for loop
   end -- end outer for loop
+
   -- We have nested loops so we can do tail recursion
-  return recursive_scan_for_nodes_for_controller(network, connections, numScanned)
+  if newToScan <= 0 then return STATUS_OK
+  else return recursive_scan_for_nodes_for_controller(network, connections, numScanned) end
 end
 
 local function create_network(controllerPosition)
@@ -229,11 +193,13 @@ local function create_network(controllerPosition)
   network.mass_storage = {}
   network.item_storage = {}
   network.storage_cache = {}
-  local status = recursive_scan_for_nodes_for_controller(network, {controllerPosition})
+  local startPos = {}
+  startPos[controllerHash] = true
+  local status = recursive_scan_for_nodes_for_controller(network, startPos)
   local errorMsg = nil
   if status == CREATE_NETWORK_STATUS_FAIL_OTHER_NETWORK then
-    errorMsg = "Cannot connect to already existing network!"
-    break_logistica_node(pos)
+    errorMsg = "Cannot create network: Would overlap with another network!"
+    break_logistica_node(controllerPosition)
   elseif status == CREATE_NETWORK_STATUS_TOO_MANY_NODES then
     errorMsg = "Controller max nodes limit of "..HARD_NETWORK_NODE_LIMIT.." nodes per network exceeded!"
   elseif status == STATUS_OK then
@@ -287,33 +253,37 @@ local function try_to_add_network(pos)
 end
 
 local function try_to_add_mass_storage_to_network(pos)
-  local connectedNetworks = {}
-  for _, adj in pairs(adjecent) do
-    local otherPos = vector.add(pos, adj)
-    local otherNetwork = logistica.get_network_id_or_nil(otherPos)
-    if otherNetwork then
-      connectedNetworks[otherNetwork] = true
-    end
-  end
-  local firstNetworId = nil
-  local numNetworks = 0
-  for k,_ in pairs(connectedNetworks) do
-    numNetworks = numNetworks + 1
-    if firstNetworId == nil then firstNetworId = k end
-  end
-  if numNetworks <= 0 then return STATUS_OK end -- nothing to connect to
-  if numNetworks >= 2 then
+  local connectedNetworks = find_adjecent_networks(pos)
+  if #connectedNetworks <= 0 then return STATUS_OK end -- nothing to connect to
+  if #connectedNetworks >= 2 then
     break_logistica_node(pos) -- swap out storage node for disabled one 
     minetest.get_meta(pos):set_string("infotext", "ERROR: cannot connect to multiple networks!")
-    return ADD_STORAGE_MULTIPLE_NETWORKS
   end
   -- else, we have 1 network, add us to it!
-  dumb_add_storage_to_network(firstNetworId, pos)
-  logistica.updateStorageCache(networks[firstNetworId])
-  return STATUS_OK
+  connectedNetworks[1].mass_storage[p2h(pos)] = true
+  logistica.updateStorageCache(connectedNetworks[1])
 end
 
 local function remove_mass_storage_from_network(pos)
+  local hash = p2h(pos)
+  local network = logistica.get_network_or_nil(pos)
+  if not network then return end
+  if not network.mass_storage[hash] then return end
+  network.mass_storage[hash] = nil
+end
+
+local function try_to_add_demander_to_network(pos)
+  local connectedNetworks = find_adjecent_networks(pos)
+  if #connectedNetworks <= 0 then return STATUS_OK end -- nothing to connect to
+  if #connectedNetworks >= 2 then
+    break_logistica_node(pos) -- swap out storage node for disabled one 
+    minetest.get_meta(pos):set_string("infotext", "ERROR: cannot connect to multiple networks!")
+  end
+  -- else, we have 1 network, add us to it!
+  connectedNetworks[1].demanders[p2h(pos)] = true
+end
+
+local function remove_demander_from_network(pos)
   local hash = p2h(pos)
   local network = logistica.get_network_or_nil(pos)
   if not network then return end
@@ -335,17 +305,15 @@ function logistica.on_cable_change(pos, oldNode)
 
   if networkEnd then
     if not placed then -- removed a network end
-      dumb_remove_from_network(pos)
+      local network = logistica.get_network_or_nil(pos)
+      if network then network.cables[p2h(pos)] = nil end
     else
-      local otherNode = minetest.get_node(connections[1])
-      if logistica.is_cable(otherNode.name) or logistica.is_controller(otherNode.name) then
-        local otherNetwork = logistica.get_network_id_or_nil(connections[1])
-        if otherNetwork then
-          dumb_add_cable_to_network(otherNetwork, pos)
-        end
+      local otherNetwork = logistica.get_network_or_nil(connections[1])
+      if otherNetwork then
+        otherNetwork.cables[p2h(connections[1])] = true
       end
     end
-    return
+    return -- was a network end, no need to do anything else
   end
 
   -- We have more than 1 connected nodes - either cables or machines, something needs recalculating
@@ -389,5 +357,15 @@ function logistica.on_storage_change(pos, oldNode)
     try_to_add_mass_storage_to_network(pos)
   else
     remove_mass_storage_from_network(pos)
+  end
+end
+
+
+function logistica.on_demander_change(pos, oldNode)
+  local placed = (oldNode == nil) -- if oldNode is nil, we placed a new one
+  if placed == true then
+    try_to_add_demander_to_network(pos)
+  else
+    remove_demander_from_network(pos)
   end
 end
