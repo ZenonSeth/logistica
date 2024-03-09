61ad4790cf4c32d829e803ecde5a696101d62249
Zenon Seth <Zenon.Seth@gmail.com>
2024-03-09 21:55:59 +0000

    Add support for i3 inventory recipes for Lava Furnace

---
+++ registration/compat_i3.lua
@@ -0,0 +1,46 @@
+if minetest.global_exists("i3") then
+
+  local L  = function(str) return "logistica:"..str end
+
+  i3.register_craft_type("logisticalavafurnace", {
+    description = "Lava Furnace [logistica]",
+    icon = "logistica_lava_furnace_front_off.png",
+  })
+
+  i3.register_craft({
+    type   = "digging",
+    result = "default:cobble 2",
+    items  = {"default:stone"},
+  })
+
+  i3.register_craft({
+		items = {"default:silver_sand", "default:ice"},
+		result = L("silverin"),
+		type = "logisticalavafurnace"
+	})
+
+	i3.register_craft({
+		items = {L("silverin"), "default:steel_ingot"},
+		result = L("silverin_plate 4"),
+		type = "logisticalavafurnace"
+	})
+
+	i3.register_craft({
+		items = {L("silverin_slice"), "default:mese_crystal_fragment"},
+		result = L("silverin_circuit"),
+		type = "logisticalavafurnace"
+	})
+
+  i3.register_craft({
+		items = {"default:glass", L("silverin_slice 6")},
+		result = L("silverin_mirror_box"),
+		type = "logisticalavafurnace"
+	})
+
+	i3.register_craft({
+		items = {L("silverin"), "default:mese_crystal"},
+		result = L("wireless_crystal"),
+		type = "logisticalavafurnace"
+	})
+
+end
