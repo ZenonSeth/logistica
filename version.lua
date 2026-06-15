a24d00ff30b3e51d4ec5c1daaf563be93527e4db
Zenon Seth <Zenon.Seth@gmail.com>
2026-06-15 06:46:26 +0100

    Up version to 2.0.4

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 logistica.VERSION = {
   major = 2,
   minor = 0,
-  patch = 3,
+  patch = 4,
 }
 
 logistica.VERSION_STRING = logistica.VERSION.major.."."..logistica.VERSION.minor.."."..logistica.VERSION.patch
