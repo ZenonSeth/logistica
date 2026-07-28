f0419b554eb36277db44c2e38a0c037c68e9027d
Zenon Seth <Zenon.Seth@gmail.com>
2026-07-28 19:47:26 +0100

    Bump version up to 2.0.12

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 logistica.VERSION = {
   major = 2,
   minor = 0,
-  patch = 11,
+  patch = 12,
 }
 
 logistica.VERSION_STRING = logistica.VERSION.major.."."..logistica.VERSION.minor.."."..logistica.VERSION.patch
